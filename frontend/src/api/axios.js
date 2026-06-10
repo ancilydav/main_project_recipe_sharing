9651c31d1ff34f4dd0c605aa8b5e39cb03fa9436
Ancily Davis <ancily18@gmail.com>
2026-06-10 17:33:41 +0530

    env variable added

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
+
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:5001/api",
+    baseURL,
 });
+
 export default axiosInstance;
